a637deddfc6397204d1354c3629e0d6ba090affa
Maik Schmidt <maikschmidt91@gmail.com>
2023-03-06 18:55:57 +0100

    Image upload functioning

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const fileUploader = require("../config/cloudinary.config");
+const Tournament = require("../models/Tournament.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+router.post("/upload/:tournamentId", fileUploader.single("imageUrl"), async (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  await Tournament.findByIdAndUpdate(req.params.tournamentId, {backgroundImage: req.file.path})
+  res.json({ fileUrl: req.file.path });
+});
 
 module.exports = router;
--- routes/tournament.routes.js
+++ routes/tournament.routes.js
@@ -350,13 +350,4 @@ router.post("/comments/delete/:id", async (req, res, next) => {
   }
 });
 
-
-router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
-  }  
-  res.json({ fileUrl: req.file.path });
-});
-
 module.exports = router;
